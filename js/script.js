f1cc0f4ff0388e5d38d61ceff575d2061bfcc679
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-09 01:35:16 +0000

    save

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 // Created by: George Khalil
 // Created on: May 2024
 // This file contains the JS functions for index.html
+
+function calculateSum() {
+  const n = parseInt(document.getElementById("numberInput").value)
+
+  if (!isNaN(n) && n > 0) {
+    const sum = (n / 2) * (n + 1)
+
+    // Using a for loop to add up numbers from 1 to the entered integer
+    for (let i = 1; i <= n; i++) {
+      sum += i
+    }
+    document.getElementById("result").innerHTML =
+      "The sum of numbers from 1 to " + n + " is " + sum + "."
+  } else {
+    document.getElementById("result").innerHTML =
+      "Please enter a positive integer."
+  }
+}
